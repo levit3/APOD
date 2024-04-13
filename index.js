4c3c84f52b1ee93588598df9e33bbcd88d1cea27
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-04-13 16:43:53 +0300

    stores API key in env and fetches data from the api

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 //we initialize every variable that we will use later on
 const header = document.getElementById("dateHeader");
 const title = document.getElementById("title");
@@ -7,3 +8,10 @@ const descriptionHeader = document.getElementById("descriptionHeader");
 const container = document.getElementById("explanation");
 const form = document.getElementById("dateSearch");
 const dateInput = form.dateInput;
+
+//we fetch data from the server
+const apiKey = process.env.API_KEY;
+fetch(`https://api.nasa.gov/planetary/apod?api_key=apiKey`)
+  .then((res) => res.json())
+  .then((data) => renderData(data)) //we pass on the data to the render function in order to render it to the DOM
+  .catch((error) => console.error(error.message)); //we catch any error messages in the console
