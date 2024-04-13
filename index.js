9a4682dd4bd46ac7c67da9e140c9c8fcd4b9539c
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-04-13 16:16:37 +0300

    initializes all the necessary variables from the html document

---
+++ index.js
@@ -0,0 +1,9 @@
+//we initialize every variable that we will use later on
+const header = document.getElementById("dateHeader");
+const title = document.getElementById("title");
+const potd = document.getElementById("potd");
+const dateHeader = document.getElementById("dateHeader");
+const descriptionHeader = document.getElementById("descriptionHeader");
+const container = document.getElementById("explanation");
+const form = document.getElementById("dateSearch");
+const dateInput = form.dateInput;
